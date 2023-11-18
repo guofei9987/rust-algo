399fdcfbca54d7cd67fbfd031446f1891f8cdbdd
guofei9987 <guofei9987@foxmail.com>
2023-11-18 20:10:54 +0800

    trie：不再使用 HashMap

--- src/history/trie_old.rs
+++ src/history/trie_old.rs
@@ -4,13 +4,12 @@
 2. 基本元素为 char，不需要用 T 来做泛型
 3. 不考虑删除关键词，因此也不需要 prev 来管理孔洞
 4. 不考虑多树合并、拆分，因此也不需要用 arena list 来管理
-5. idx = 0 就是 根节点
-6. 前一个版本的 next_idx 使用的是 HashMap<u8, usize>，在频繁索引时有一定的性能消耗，这个版本使用 [usize; 256]
+5. idx = 0 就是 跟节点
  */
+use std::collections::HashMap;
 
 struct Node {
-    next_idx: [usize; 256],
-    has_child: bool,
+    next_idx: HashMap<u8, usize>,
     is_word: bool,
 }
 
@@ -29,18 +28,19 @@ impl Trie {
     }
 
     fn add_node(&mut self) -> usize {
-        self.nodes.push(Node { next_idx: [0; 256], has_child: false, is_word: false });
-        self.nodes.len() - 1
+        let len = self.nodes.len();
+        self.nodes.push(Node { next_idx: HashMap::new(), is_word: false });
+        len
     }
 
     pub fn insert(&mut self, word: &str) {
         let mut curr_idx: usize = 0;
-        for chr in word.bytes() {
-            if self.nodes[curr_idx].next_idx[chr as usize] == 0 {
-                self.nodes[curr_idx].next_idx[chr as usize] = self.add_node();
-                self.nodes[curr_idx].has_child = true;
+        for chr in word.as_bytes() {
+            if !self.nodes[curr_idx].next_idx.contains_key(chr) {
+                let new_idx = self.add_node();
+                self.nodes[curr_idx].next_idx.insert(*chr, new_idx);
             }
-            curr_idx = self.nodes[curr_idx].next_idx[chr as usize]
+            curr_idx = self.nodes[curr_idx].next_idx[chr]
         }
         self.nodes[curr_idx].is_word = true;
         return;
@@ -50,11 +50,11 @@ impl Trie {
         let mut curr_idx = 0;
         let mut idx = 0;
         let text = text.as_bytes();
-        while self.nodes[curr_idx].has_child && idx < text.len() {
-            let chr = text[idx] as usize;
-            if self.nodes[curr_idx].next_idx[chr] == 0 {
-                return false;
-            } else { curr_idx = self.nodes[curr_idx].next_idx[chr] }
+        while self.nodes[curr_idx].next_idx.len() > 0 && idx < text.len() {
+            match self.nodes[curr_idx].next_idx.get(&text[idx]) {
+                Some(next_idx) => curr_idx = *next_idx,
+                None => return false
+            }
             idx += 1;
         }
 
